e2dcc858ee0a326aa1920a724599cf26e4d645f7
Christopher Coté <Christopher Cot>
2017-08-19 11:39:54 -0500

    make all calls asynchronous

--- lib/radio_thermostat.ex
+++ lib/radio_thermostat.ex
@@ -1,85 +1,90 @@
 defmodule RadioThermostat do
-    use GenServer
-    require Logger
+  use GenServer
+  require Logger
 
-    @days %{:mon => 0, :tue => 1, :wed => 2, :thu => 3, :fri => 4, :sat => 5, :sun => 6}
-    @on_off %{:on => 2, :off => 1}
-    @hold %{:on => 1, :off => 0}
-    @tmode %{:heat => 1, :cool => 2, :auto => 3}
-    @fmode %{:auto => 0, :auto_circulate => 1, :on => 2, :off => 0}
-    @program_mode %{:program_a => 0, :program_b => 1, :vacation => 2, :holiday => 3}
-    @resources %{
-        :state => %{:key => "", :get => true, :post => false, :post_url => ""},
-        :operating_mode => %{:key => "tmode", :get => true, :post => true, :post_url => "", :value => @tmode},
-        :operating_state => %{:key => "tstate", :get => true, :post => false, :post_url => ""},
-        :temp => %{:key => "temp", :get => true, :post => false, :post_url => ""},
-        :fan => %{:key => "fmode", :get => true, :post => true, :post_url => "", :value => @fmode},
-        :hold => %{:key => "hold", :get => true, :post => true, :post_url => "", :value => @hold},
-        :temporary_heat => %{:key => "t_heat", :get => false, :post => true, :post_url => "", :value => :float},
-        :temporary_cool => %{:key => "t_cool", :get => false, :post => true, :post_url => "", :value => :float},
-        :absolute_heat => %{:key => "a_heat", :get => false, :post => true, :post_url => "", :value => :float},
-        :absolute_cool => %{:key => "a_cool", :get => false, :post => true, :post_url => "", :value => :float},
-        :time => %{:key => "time", :get => true, :post => true, :post_url => "", :value => :map},
-        :program_mode => %{:key => "program_mode", :get => false, :post => true, :post_url => "", :value => @program_mode},
-        :price_message => %{:key => "", :get => false, :post => true, :post_url => "/pma", :value => :map}
-    }
+  @days %{:mon => 0, :tue => 1, :wed => 2, :thu => 3, :fri => 4, :sat => 5, :sun => 6}
+  @on_off %{:on => 2, :off => 1}
+  @hold %{:on => 1, :off => 0}
+  @tmode %{:heat => 1, :cool => 2, :auto => 3}
+  @fmode %{:auto => 0, :auto_circulate => 1, :on => 2, :off => 0}
+  @program_mode %{:program_a => 0, :program_b => 1, :vacation => 2, :holiday => 3}
+  @resources %{
+    :state => %{:key => "", :get => true, :post => false, :post_url => ""},
+    :operating_mode => %{:key => "tmode", :get => true, :post => true, :post_url => "", :value => @tmode},
+    :operating_state => %{:key => "tstate", :get => true, :post => false, :post_url => ""},
+    :temp => %{:key => "temp", :get => true, :post => false, :post_url => ""},
+    :fan => %{:key => "fmode", :get => true, :post => true, :post_url => "", :value => @fmode},
+    :hold => %{:key => "hold", :get => true, :post => true, :post_url => "", :value => @hold},
+    :temporary_heat => %{:key => "t_heat", :get => false, :post => true, :post_url => "", :value => :float},
+    :temporary_cool => %{:key => "t_cool", :get => false, :post => true, :post_url => "", :value => :float},
+    :absolute_heat => %{:key => "a_heat", :get => false, :post => true, :post_url => "", :value => :float},
+    :absolute_cool => %{:key => "a_cool", :get => false, :post => true, :post_url => "", :value => :float},
+    :time => %{:key => "time", :get => true, :post => true, :post_url => "", :value => :map},
+    :program_mode => %{:key => "program_mode", :get => false, :post => true, :post_url => "", :value => @program_mode},
+    :price_message => %{:key => "", :get => false, :post => true, :post_url => "/pma", :value => :map}
+  }
 
-    def start_link(url) do
-        GenServer.start_link(__MODULE__, url)
-    end
+  def start_link(url) do
+    GenServer.start_link(__MODULE__, url)
+  end
 
-    def system(rt, path \\ "") do
-        GenServer.call(rt, {:get, "/sys" <> path})
-    end
+  def system(rt, path \\ "") do
+    GenServer.call(rt, {:get, "/sys" <> path})
+  end
 
-    def state(rt) do
-        get(rt, :state)
-    end
+  def state(rt) do
+    get(rt, :state)
+  end
 
-    def set(rt, resource, value) do
-        with %{:key => key, :post => post, :post_url => path, :value => parser} <- @resources[resource],
-            true <- post do
-            val =
-                case parser do
-                    :float -> value
-                    :map -> value
-                    other -> other[value]
-                end
-            GenServer.call(rt, {:post, "/tstat" <> path, %{key => val}}, 20000)
+  def set(rt, resource, value) do
+    with %{:key => key, :post => post, :post_url => path, :value => parser} <- @resources[resource],
+    true <- post do
+      val =
+        case parser do
+          :float -> value
+          :map -> value
+          other -> other[value]
         end
+        GenServer.call(rt, {:post, "/tstat" <> path, %{key => val}, resource}, 5000)
     end
+  end
 
-    def get(rt, resource) do
-        with %{:key => key, :get => get} <- @resources[resource],
-            true <- get,
-            do: GenServer.call(rt, {:get, "/tstat/" <> key}, 20000)
-    end
+  def get(rt, resource) do
+    with %{:key => key, :get => get} <- @resources[resource],
+    true <- get,
+    do: GenServer.call(rt, {:get, "/tstat/" <> key, resource}, 5000)
+  end
 
-    def init(url) do
-        {:ok, %{:url => url}}
-    end
+  def init(url) do
+    {:ok, %{:url => url}}
+  end
 
-    def handle_call({:get, path}, _from, state) do
-        case RadioThermostat.Client.do_get(path, state.url) do
-            {:ok, response} ->
-                {:reply, {:ok, response}, state}
-            {:error, reason} ->
-                {:reply, {:error, reason}, state}
-        end
-    end
+  def handle_call({:get, path, resource}, {caller, _}, state) do
+    {:ok, pid} = Task.start(fn ->
+      case RadioThermostat.Client.do_get(path, state.url) do
+        {:ok, response} ->
+          send(caller, {resource, :ok, response})
+        {:error, reason} ->
+          send(caller, {resource, :error, reason})
+      end
+    end)
+    {:reply, pid, state}
+  end
 
-    def handle_call({:post, path, value}, _from, state) do
-        case RadioThermostat.Client.do_post(path, state.url, value) do
-            {:ok, response} ->
-                {:reply, {:ok, response}, state}
-            {:error, reason} ->
-                {:reply, {:error, reason}, state}
-        end
-    end
+  def handle_call({:post, path, value, resource}, {caller, _}, state) do
+    {:ok, pid} = Task.start(fn ->
+      case RadioThermostat.Client.do_post(path, state.url, value) do
+        {:ok, response} ->
+          send(caller, {resource, :ok, response})
+        {:error, reason} ->
+          send(caller, {resource, :error, reason})
+      end
+    end)
+    {:reply, pid, state}
+  end
 
-    def handle_info(:timeout, state) do
-      Logger.error "Call timed out. Keep on truckin..."
-      {:noreply, state}
-    end
-
+  def handle_info(:timeout, state) do
+    Logger.error "Call timed out. Keep on truckin..."
+    {:noreply, state}
+  end
 end
--- lib/radio_thermostat/client.ex
+++ lib/radio_thermostat/client.ex
@@ -1,29 +1,29 @@
 defmodule RadioThermostat.Client do
-    use HTTPoison.Base
+  use HTTPoison.Base
 
-    def do_get(path, url, parameters \\ %{}) do
-        case RadioThermostat.Client.get(url <> path, [], params: parameters, timeout: 20000, recv_timeout: 20000) do
-            {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-                {:ok, body |> Poison.Parser.parse!}
-            {:ok, %HTTPoison.Response{body: body, status_code: status_code}} when status_code > 400 ->
-                {:error, body}
-            {:error, reason} ->
-                {:error, reason}
-        end
+  def do_get(path, url, parameters \\ %{}) do
+    case RadioThermostat.Client.get(url <> path, [], params: parameters, timeout: 5000, recv_timeout: 5000) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {:ok, body |> Poison.Parser.parse!}
+      {:ok, %HTTPoison.Response{body: body, status_code: status_code}} when status_code > 400 ->
+        {:error, body}
+      {:error, reason} ->
+        {:error, reason}
     end
+  end
 
-    def do_post(path, url, body) do
-        {:ok, body} = Poison.encode(body)
-        case RadioThermostat.Client.post(url <> path, body, timeout: 20000, recv_timeout: 20000) do
-            {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-                case body |> Poison.Parser.parse! do
-                    %{"success" => 0} = payload -> {:ok, payload}
-                    payload -> {:error, payload}
-                end
-            {:ok, %HTTPoison.Response{body: body, status_code: status_code}} when status_code > 400 ->
-                {:error, body}
-            {:error, reason} ->
-                {:error, reason}
+  def do_post(path, url, body) do
+    {:ok, body} = Poison.encode(body)
+    case RadioThermostat.Client.post(url <> path, body, timeout: 5000, recv_timeout: 5000) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        case body |> Poison.Parser.parse! do
+          %{"success" => 0} = payload -> {:ok, payload}
+          payload -> {:error, payload}
         end
+      {:ok, %HTTPoison.Response{body: body, status_code: status_code}} when status_code > 400 ->
+        {:error, body}
+      {:error, reason} ->
+        {:error, reason}
     end
+  end
 end
